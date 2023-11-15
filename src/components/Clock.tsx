b7fccb7a145bd1f3931047e97cd6e40e4c61bae0
RockyPHER <davidsonjunior10@gmail.com>
2023-11-15 12:12:17 -0300

    Created function to add task element onClick

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,10 +1,10 @@
 interface ClockProps {
-    minutes: number
-    seconds: number
+    minutes: string
+    seconds: string
 }
 export function Clock({ minutes, seconds}: ClockProps) {
     return (
-        <div className="flex">
+        <div className="flex ">
             <span className="flex justify-center"><div className="text-9xl p-2 m-2 bg-gray-600 text-white rounded-xl">{minutes}</div></span>
             <span className="flex justify-center"><div className="text-9xl p-1 m-2 text-white rounded-xl">:</div></span>
             <span className="flex justify-center"><div className="text-9xl p-2 m-2 bg-gray-600 text-white rounded-xl">{seconds}</div></span>
