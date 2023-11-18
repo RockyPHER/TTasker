a03b38b9bba27444dff9b18b201c5cd48ed65b6b
RockyPHER <davidsonjunior10@gmail.com>
2023-11-18 02:29:55 -0300

    some changes

--- app/ui/Stack.tsx
+++ app/ui/Stack.tsx
@@ -2,13 +2,13 @@ import Task from "./Task";
 
 export default function Stack() {
     return (
-        <div className="bg-transparent border-slate-500 border-2 border-solid h-[96.5vh] w-1/4 m-3 rounded-xl shadow-xl">
+        <div className="bg-gray-700 bg-opacity-50 border-gray-100 border-opacity-40 shadow border-2 border-solid h-[96.5vh] w-1/4 m-3 rounded-xl shadow-xl">
                 <div className="flex justify-center w-full"><h1 className="text-3xl p-5 text-white">Stack</h1></div>
                 <div className="flex flex-col w-full h-[85%] justify-between">
                     <ul>
                         <Task time={"12:00"} title={"Like tears"} description={"In the rain"} />
                     </ul>
-                    <div className="flex w-full justify-center"><button className=" hover:bg-slate-500 active:bg-slate-600 text-3xl px-2 text-white bg-slate-400 rounded-3xl">+</button></div>
+                    <div className="flex w-full justify-center"><button className="hover:bg-slate-500 active:bg-slate-600 text-3xl px-2 text-white bg-slate-400 rounded-3xl">+</button></div>
                 </div>
         </div>
     )
--- app/ui/Task.tsx
+++ app/ui/Task.tsx
@@ -6,12 +6,12 @@ interface TaskProps {
 export default function Task({time, title, description} : TaskProps) {
 
     return (
-        <li className="flex flex-col bg-gray-100 p-3 mx-3 rounded space-y-3">
+        <li className="flex flex-col bg-white p-3 mx-3 rounded space-y-3">
             <div className="flex justify-between text-black items-start">
                 <span className="p-1 m-1">{title}</span>
-                <span className="bg-gray-300 p-1 rounded m-1">{time}</span>
+                <span className="bg-gray-200 p-1 rounded m-1">{time}</span>
             </div>
-            <div className="bg-gray-300 p-3 rounded">
+            <div className="bg-gray-200 p-3 rounded">
                 <span className="text-black ">{description}</span>
             </div>
         </li>
